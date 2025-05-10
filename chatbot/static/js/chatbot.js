bce77c4bc2985830d467f3af9c60615cafe9c22c
Dain Lee <109786068+daainn@users.noreply.github.com>
2025-05-10 18:26:11 +0900

    refact: chatbot.js code

--- chatbot/static/js/chatbot.js
+++ chatbot/static/js/chatbot.js
@@ -31,64 +31,13 @@ function setLang(button, lang) {
   }
 }
 
-// function handleQuestion(event) {
-//   event.preventDefault();
-//   const input = document.getElementById('userQuestion');
-//   const question = input.value.trim();
-//   if (!question) return;
-
-//   const chatContent = document.getElementById('chatContent');
-
-//   const userDiv = document.createElement('div');
-//   userDiv.className = 'message user-message';
-//   userDiv.textContent = question;
-//   chatContent.appendChild(userDiv);
-
-//   const botDiv = document.createElement('div');
-//   botDiv.className = 'message bot-response';
-//   botDiv.textContent = '...';  // 로딩 상태 표시
-//   chatContent.appendChild(botDiv);
-
-//   // FastAPI 서버에 질의 요청
-//   fetch('https://1p7peu7026lfk0-8002.proxy.runpod.net/chat', {  // FastAPI URL로 요청
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({ text: question }),  // 질문을 JSON으로 보내기
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     const generatedText = data.generated_text;  // FastAPI 응답
-//     const apiKey = data.api_key;  // FastAPI에서 받은 API 키
-//     console.log("API Key: ", apiKey);  // API 키 사용 예시
-//     botDiv.innerHTML = `
-//       ${generatedText}
-//       <br><br>
-//       <img src="{% static 'image/winter-landscape.png' %}" alt="겨울산수" class="response-image">
-//       <audio controls class="response-audio">
-//         <source src="{% static 'sample.mp3' %}" type="audio/mpeg">
-//         오디오 지원이 불가합니다.
-//       </audio>
-//     `;
-//     chatContent.appendChild(botDiv);
-//   })
-//   .catch(error => {
-//     botDiv.innerText = 'Error: 서버와의 통신이 실패했습니다.';
-//   });
-
-//   input.value = '';  // 입력창 비우기
-//   chatContent.scrollTop = chatContent.scrollHeight;  // 스크롤 하단으로 이동
-// }
 
 window.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
   const lang = urlParams.get("lang") || localStorage.getItem("selectedLang") || "ko";
 
-  // 선택된 언어를 localStorage에 저장하여 유지
   localStorage.setItem("selectedLang", lang);
 
-  // 언어 설정 함수 호출
   const langButton = Array.from(document.querySelectorAll(".top-bar button"))
     .find(btn => btn.textContent.toLowerCase().includes(lang === "ko" ? "한국" : lang === "en" ? "english" : "日本"));
 
