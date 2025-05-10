7b6d49fd5a97ed4f742b04a9fc05f2bbe0680790
Dain Lee <109786068+daainn@users.noreply.github.com>
2025-05-10 18:32:44 +0900

    refact: settings.py code

--- muse/settings.py
+++ muse/settings.py
@@ -5,25 +5,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY', '')
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '194.68.245.149',   # RunPod 퍼블릭 IP
-    'localhost',        # 내부 로컬 호출
-    '127.0.0.1',        # 루프백 IP
-    '0.0.0.0',          # 모든 주소에서 접근 허용
-    'muse.com',         # 도메인 연결 시 (선택 사항)
+    '194.68.245.149', 
+    'localhost', 
+    '127.0.0.1', 
+    '0.0.0.0',    
+    'muse.com',    
 ]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -47,25 +42,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'muse.urls'
 
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [BASE_DIR / 'chatbot' / 'templates'], 
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',  # 이 부분이 템플릿 폴더를 올바르게 포함해야 합니다.
+            BASE_DIR / 'templates', 
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -86,20 +67,12 @@ STATICFILES_DIRS = [
 
 WSGI_APPLICATION = 'muse.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -116,10 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -129,13 +98,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
