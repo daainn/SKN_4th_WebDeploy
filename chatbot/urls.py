9f6d58d8035e71cc8064b46cd88ebd229051035b
Dain Lee <109786068+daainn@users.noreply.github.com>
2025-05-10 18:31:16 +0900

    refact: urls.py code

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,10 +1,9 @@
-# chatbot/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('logo/', views.logo_page, name='logo_page'),
-    path('landing/', views.landing_page, name='landing_page'),  # 랜딩 페이지 URL
+    path('landing/', views.landing_page, name='landing_page'), 
     path('explain/', views.explain_page, name='explain_page'),
     path('chatbot/', views.chatbot_page, name='chatbot_page'),
 ]
