6ce3ddd036a796268dbe10f3a0c3de4604afbd9f
Dain Lee <109786068+daainn@users.noreply.github.com>
2025-05-10 18:33:04 +0900

    Update urls.py

--- muse/urls.py
+++ muse/urls.py
@@ -4,9 +4,9 @@ from chatbot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.logo_page, name='logo_page'),  # '/' 경로에서 'logo_page'로 설정
-    path('logo/', views.logo_page, name='logo_page'),  # 'logo/' 경로도 'logo_page'로 연결
-    path('landing/', views.landing_page, name='landing_page'),  # 'landing/' 경로는 'landing_page'로 연결
+    path('', views.logo_page, name='logo_page'),  
+    path('logo/', views.logo_page, name='logo_page'), 
+    path('landing/', views.landing_page, name='landing_page'), 
     path('explain/', views.explain_page, name='explain_page'),
     path('chatbot/', views.chatbot_page, name='chatbot_page'),
 ]
